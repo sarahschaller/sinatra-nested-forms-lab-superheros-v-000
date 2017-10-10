c29a88e00445bb6e0ce17a51b010e80b3263f42b
Sarah Schaller <sarcsch@aol.com>
2017-10-10 19:08:14 +0000

    automatically backed up by learn

--- app/models/superhero.rb
+++ app/models/superhero.rb
@@ -1,4 +1,18 @@
 class SuperHero
+  attr_reader :name, :power, :biography
+
+  SUPERHEROES = []
+
+  def initialize(params)
+    @name = params[:name]
+    @grade = params[:power]
+    @biography = params[:biography]
+    SUPERHEROES << self 
+  end 
+
+  def self.all 
+    SUPERHEROES
+  end
 
 
 end
